ebf16b334e80ea3df6b976e84f96541956ab3dfd
zoubingwu <zoubingwu@gmail.com>
2021-04-22 12:41:59 +0800

    use windicss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { visualizer } from 'rollup-plugin-visualizer';
+import windicss from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(), windicss()],
+  define: {
+    'process.env': {},
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer()
+      ]
+    }
+  }
 })
