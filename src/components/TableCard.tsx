f4acc7e43b76e383a71bed3178e838da07d0062d
zoubingwu <zoubingwu@gmail.com>
2021-05-07 18:36:07 +0800

    add react draggable

--- src/components/TableCard.tsx
+++ src/components/TableCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Draggable from "react-draggable";
 import { Table } from "../core/Table";
 
 export const TableCard: React.FC<{
@@ -6,15 +7,17 @@ export const TableCard: React.FC<{
 }> = ({ data }) => {
   const { name, position, columns } = data;
   return (
-    <div
-      className="absolute shadow-sm border rounded p-2 w-72"
-      style={{ left: position?.x, top: position?.y }}
-    >
-      <div>{name}</div>
+    <Draggable>
+      <div
+        className="absolute shadow-sm border rounded p-2 w-72"
+        style={{ left: position?.x, top: position?.y }}
+      >
+        <div>{name}</div>
 
-      {columns.map((c) => (
-        <div key={c.name}>{c.name}</div>
-      ))}
-    </div>
+        {columns.map((c) => (
+          <div key={c.name}>{c.name}</div>
+        ))}
+      </div>
+    </Draggable>
   );
 };
