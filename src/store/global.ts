7fd037fe465c89a129010fa1db55587968f5aafe
zoubingwu <zoubingwu@gmail.com>
2021-06-03 17:47:21 +0800

    rename target slice to global

---
+++ src/store/global.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { allTargets } from "../codegen";
+import { SQLTarget } from "../codegen/SQL";
+import { TargetLanguage } from "../codegen/TargetLanguage";
+
+interface GlobalState {
+  currentTarget: TargetLanguage;
+  showCode: boolean;
+}
+
+const initialState: GlobalState = {
+  currentTarget: new SQLTarget(),
+  showCode: true,
+};
+
+export const globalOptionsSlice = createSlice({
+  name: "options",
+  initialState,
+  reducers: {
+    pickTarget(state, action) {
+      const nextTarget = allTargets[action.payload];
+      if (nextTarget) {
+        state.currentTarget = nextTarget;
+      }
+    },
+
+    toggleCode(state) {
+      state.showCode = !state.showCode;
+    },
+  },
+});
+
+export const actions = globalOptionsSlice.actions;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { diagramSlice } from "./diagram";
-import { targetSlice } from "./target";
+import { diagramSlice, actions as diagramActions } from "./diagram";
+import { globalOptionsSlice, actions as globalActions } from "./global";
 
 export const store = configureStore({
   reducer: {
-    target: targetSlice.reducer,
+    globalOptions: globalOptionsSlice.reducer,
     diagram: diagramSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
@@ -22,3 +22,8 @@ export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const actions = {
+  ...diagramActions,
+  ...globalActions,
+};
