1b196691ad1a9b0b9a974729b7ad1b07003c34ff
zoubingwu <zoubingwu@gmail.com>
2021-06-07 01:53:07 +0800

    fix: only run logger in dev mode

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { createLogger } from "redux-logger";
 import { diagramSlice } from "./diagram";
 import { globalOptionsSlice } from "./global";
-
-//@ts-ignore
-const logger = (s) => (next) => (action) => {
-  console.log(action);
-  next(action);
-  console.log(s.getState());
-};
 
 export const store = configureStore({
   reducer: {
     globalOptions: globalOptionsSlice.reducer,
     diagram: diagramSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware({
       serializableCheck: false,
-    }).concat(logger),
+    });
+
+    if (import.meta.env.DEV) {
+      const logger = createLogger({
+        duration: true,
+        collapsed: true,
+      });
+      middlewares.push(logger);
+    }
+
+    return middlewares;
+  },
+
   devTools: true,
 });
 
