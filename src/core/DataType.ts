752d52f46bcedb95f2fcfb846c8356d1d9833be6
zoubingwu <zoubingwu@gmail.com>
2021-06-10 19:46:21 +0800

    fix: add more column data types

--- src/core/DataType.ts
+++ src/core/DataType.ts
@@ -1,3 +1,22 @@
-export const dataTypes = ["INT", "VARCHAR", "DATETIME"] as const;
+export const dataTypes = [
+  // int types
+  "INT",
+  "BIGINT",
+  "DECIMAL",
+
+  // string types
+  "VARCHAR",
+  "TEXT",
+
+  // date types
+  "DATETIME",
+  "TIME",
+  "TIMESTAMP",
+
+  // binary types
+  "BINARY",
+  "BLOB",
+  "VARBINARY",
+] as const;
 
 export type DataType = typeof dataTypes[number];
