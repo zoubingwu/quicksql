7e0c01b2cdfb04048e30d5dfcb3d74f42e8caf3d
zoubingwu <zoubingwu@gmail.com>
2021-06-19 19:58:28 +0800

    build: minify html

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { visualizer } from "rollup-plugin-visualizer";
 import windicss from "vite-plugin-windicss";
+import { minifyHtml } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), windicss()],
+  plugins: [reactRefresh(), windicss(), minifyHtml()],
   define: {
     "process.env": {},
   },
