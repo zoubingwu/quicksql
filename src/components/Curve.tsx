5ff068be498260dac3465796e87d8c28ecb35797
zoubingwu <zoubingwu@gmail.com>
2021-06-20 03:41:10 +0800

    fix: fix jsx attribute

--- src/components/Curve.tsx
+++ src/components/Curve.tsx
@@ -66,7 +66,7 @@ export function Curve({
         stroke="black"
         fill="transparent"
         {...props}
-        marker-end="url(#head)"
+        markerEnd="url(#head)"
         strokeDasharray="11,5"
       />
 
